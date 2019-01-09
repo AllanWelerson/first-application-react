278156ee19f6ef6d6a57154472e2947023b79a52
= <allanwelerson@gmail.com>
2019-01-09 01:03:49 -0200

    Updates

---
+++ precache-manifest.086fe07e06eb0c9c205f249771afa170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-application-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7bee3d361ae135b8a2b",
+    "url": "/first-application-react/static/js/runtime~main.b7bee3d3.js"
+  },
+  {
+    "revision": "d1d7b600e0878802e388",
+    "url": "/first-application-react/static/js/main.d1d7b600.chunk.js"
+  },
+  {
+    "revision": "9d2723580550321d3972",
+    "url": "/first-application-react/static/js/1.9d272358.chunk.js"
+  },
+  {
+    "revision": "d1d7b600e0878802e388",
+    "url": "/first-application-react/static/css/main.20dae629.chunk.css"
+  },
+  {
+    "revision": "4faf540c4c1e3f77bd9da04b657a745a",
+    "url": "/first-application-react/index.html"
+  }
+];
